77fad714f01052c1979b0051cc72c573d7c5280c
Riccardo <riccardospadaro58@gmail.com>
2026-05-30 14:45:41 +1000

    byte refactoring

--- WaveFunctionCollapse/Assets/Scripts/WFC/Node.cs
+++ WaveFunctionCollapse/Assets/Scripts/WFC/Node.cs
@@ -1,41 +1,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : IHeapItem<Node>
+public struct NodeInfo
 {
-    public List<TileWFC> possibleTiles;
-    public int Entropy => possibleTiles.Count;
-    public int collapsedNeighbours;
-    public TileWFC nodeTile;
-    public bool IsCollapsed => nodeTile != null;
-    int heapIndex;
-    public Vector2 nodePos;
+    public byte possibleTiles;
+    public int entropy;
+    public byte tile;
     public int x;
     public int y;
 
-    public Node(Vector2 pos, IEnumerable<TileWFC> tiles, int x, int y)
+    public NodeInfo(int x, int y, byte possibleTiles)
     {
-        nodePos = pos;
-        possibleTiles = new(tiles);
         this.x = x;
         this.y = y;
+        this.possibleTiles = possibleTiles;
+        entropy = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            if((possibleTiles & 1 << i) != 0)
+                entropy++;
+        }
+
+        tile = 0;
+    }
+}
+
+public class Node : IHeapItem<Node>
+{
+    public NodeInfo NodeInfo => nodeInfo;
+    NodeInfo nodeInfo;
+    int heapIndex;
+    public Vector2 nodePos;
+
+    public Node(Vector2 pos, NodeInfo nodeInfo)
+    {
+        nodePos = pos;
+        this.nodeInfo = nodeInfo;
     }
 
-    public void ReduceEntropy()
+    public void Collapse()
     {
-        List<TileWFC> possibleTilesDup = new(possibleTiles);
-        foreach (TileWFC tile in possibleTilesDup)
-            if(tile != nodeTile)
-                possibleTiles.Remove(tile);
+        if(nodeInfo.possibleTiles == 0)
+            throw new System.IndexOutOfRangeException();
+
+        if(nodeInfo.entropy == 1)
+        {
+            nodeInfo.tile = nodeInfo.possibleTiles;
+            return;
+        }
+
+        int[] positions = new int[8];
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            if((nodeInfo.possibleTiles & 1 << i) != 0)
+            {
+                positions[count] = i;
+                count++;
+            }
+        }
+
+        nodeInfo.tile = (byte)(1 << positions[Random.Range(0, count)]);
+        nodeInfo.possibleTiles = nodeInfo.tile;
+        nodeInfo.entropy = 1;
     }
 
     public int HeapIndex { get => heapIndex; set => heapIndex = value; }
 
     public int CompareTo(Node other)
     {
-        int compare = Entropy.CompareTo(other.Entropy);
-        if(compare == 0)
-            return collapsedNeighbours.CompareTo(other.collapsedNeighbours);
+        int compare = nodeInfo.entropy.CompareTo(nodeInfo.entropy);
         return -compare;
     }
 }
--- WaveFunctionCollapse/Assets/Scripts/WFC/WFC.cs
+++ WaveFunctionCollapse/Assets/Scripts/WFC/WFC.cs
@@ -13,14 +13,33 @@ public class WFC : MonoBehaviour
     public float gridSizeY = 60f;
     public int NodesAmountX => Mathf.RoundToInt(gridSizeX / NodeDiameter);
     public int NodesAmountY => Mathf.RoundToInt(gridSizeY / NodeDiameter);
+
+    byte[] compat;
+
     Node[,] grid;
     public float timeToGenerate;
 
     Heap<Node> nodesToCollapse;
     readonly Stack<Node> nodesStack = new();
 
+    readonly List<Vector2Int> directions = new()
+    {
+        new(0, 1),
+        new(1, 0),
+        new(0, -1),
+        new(-1, 0)
+    };
+
+    // void Start()
+    // {
+    //     int a = 1;
+    //     int b = a << 1;
+    //     UnityEngine.Debug.Log(b);
+    // }
+
     public void WaveFunctionCollapse()
     {
+        GetTilesCompat();
         CreateGrid();
         ConvertArrayToHeap();
         Stopwatch sw = new();
@@ -28,6 +47,31 @@ public class WFC : MonoBehaviour
         StartWaveFunctionCollapse();
         sw.Stop();
         timeToGenerate = sw.ElapsedMilliseconds / 1000f;
+    }
+
+    void GetTilesCompat()
+    {
+        compat = new byte[tiles.Count * 4];
+
+        for (int i = 0; i < tiles.Count; i++)
+        {
+            TileWFC tile = tiles[i];
+
+            for (int d = 0; d < directions.Count; d++)
+            {
+                byte compTiles = 0;
+
+                for (int j = 0; j < tiles.Count; j++)
+                {
+                    TileWFC compTile = tiles[j];
+
+                    if(tile.GetSocket(d) == compTile.GetSocket((d + 2) % directions.Count))
+                        compTiles |= (byte)(1 << j);
+                }
+
+                compat[i * 4 + d] = compTiles;
+            }
+        }
     }
 
     void CreateGrid()
@@ -42,23 +86,25 @@ public class WFC : MonoBehaviour
                 float xPos = nodeRadius + NodeDiameter * x;
                 float yPos = nodeRadius + NodeDiameter * y;
                 Vector2 nodePos = new Vector2(xPos, yPos) + bottomLeft;
-                Node node = new(nodePos, tiles, x, y);
+                NodeInfo nodeInfo = new(x, y, 0b11111111);
 
                 if(x == 0 || x == NodesAmountX - 1 || y == 0 || y == NodesAmountY - 1)
                 {
                     for (int i = 0; i < 4; i++)
                     {
-                        if (!TryGetNeighborFromDirection(i, node, out _))
+                        if (!HasNeighbour(i, x, y))
                         {
-                            foreach (TileWFC tile in tiles)
-                            {
-                                if(tile.GetSocket(i) > 0)
-                                node.possibleTiles.Remove(tile);
-                            }
-                            node.collapsedNeighbours++;
+                            // foreach (TileWFC tile in tiles)
+                            // {
+                            //     if(tile.GetSocket(i) > 0)
+                            //     node.possibleTiles.Remove(tile);
+                            // }
+                            // node.collapsedNeighbours++;
                         }
                     }
                 }
+
+                Node node = new(nodePos, nodeInfo);
                 grid[x, y] = node;
             }
         }
@@ -78,49 +124,32 @@ public class WFC : MonoBehaviour
         }
     }
 
-    bool TryGetNeighborFromDirection(int socketDir, Node currentNode, out Node neighbour)
+    bool TryGetNeighbourFromDirection(int socketDir, Node currentNode, out Node neighbour)
     {
-        switch (socketDir)
+        Vector2Int direction = directions[socketDir];
+        int neighbourX = currentNode.NodeInfo.x + direction.x;
+        int neighbourY = currentNode.NodeInfo.y + direction.y;
+
+        if(neighbourX < 0 || neighbourX >= NodesAmountX || neighbourY < 0 || neighbourY >= NodesAmountY)
         {
-            case 0:
-                int up = currentNode.y + 1;
-                if(up >= NodesAmountY)
-                {
-                    neighbour = null;
-                    return false;
-                }
-                neighbour = grid[currentNode.x, up];
-                return true;
-            case 1:
-                int right = currentNode.x + 1;
-                if (right >= NodesAmountX)
-                {
-                    neighbour = null;
-                    return false;
-                }
-                neighbour = grid[right, currentNode.y];
-                return true;
-            case 2:
-                int down = currentNode.y - 1;
-                if(down < 0)
-                {
-                    neighbour = null;
-                    return false;
-                }
-                neighbour = grid[currentNode.x, down];
-                return true;
-            case 3:
-                int left = currentNode.x - 1;
-                if (left < 0)
-                {
-                    neighbour = null;
-                    return false;
-                }
-                neighbour = grid[left, currentNode.y];
-                return true;
-            default:
-                throw new System.ArgumentOutOfRangeException();
+            neighbour = null;
+            return false;
         }
+
+        neighbour = grid[neighbourX, neighbourY];
+        return true;
+    }
+
+    bool HasNeighbour(int dir, int x, int y)
+    {
+        Vector2Int direction = directions[dir];
+        int neighbourX = x + direction.x;
+        int neighbourY = y + direction.y;
+
+        if(neighbourX < 0 || neighbourX >= NodesAmountX || neighbourY < 0 || neighbourY >= NodesAmountY)
+            return false;
+
+        return true;
     }
 
     void StartWaveFunctionCollapse()
@@ -130,66 +159,70 @@ public class WFC : MonoBehaviour
         while(nodesToCollapse.HeapSize > 0)
         {
             currentNode = nodesToCollapse.RemoveFirst();
+            currentNode.Collapse();
 
-            //set the tile of the current node
-            int tileIndex = currentNode.possibleTiles.Count > 1 ? Random.Range(0, currentNode.possibleTiles.Count) : 0;
-            currentNode.nodeTile = currentNode.possibleTiles[tileIndex];
-            currentNode.ReduceEntropy();
-            Instantiate(currentNode.nodeTile.Object, currentNode.nodePos, Quaternion.identity, transform);
-            
-            for (int i = 0; i < 4; i++)
-            {
-                if(TryGetNeighborFromDirection(i, currentNode, out Node neighbour))
-                    neighbour.collapsedNeighbours++;
-            }
 
-            if(nodesToCollapse.HeapSize <= 0)
-                break;
-
-            nodesStack.Push(currentNode);
-            
-            //Propagate the collapse to the neighbours
-            if(nodesStack.Count > 0)
-                PropagateCollapse();
-            
         }
-    }
 
-    void PropagateCollapse()
-    {
-        List<int> possibleSockets = new();
-        List<TileWFC> neighbourTiles;
-        while(nodesStack.Count > 0)
-        {
-            Node currentNode = nodesStack.Pop();
-            for (int i = 0; i < 4; i++)
-            {
-                if(!TryGetNeighborFromDirection(i, currentNode, out Node neighbour) || neighbour.IsCollapsed)
-                    continue;
-                
-                foreach (TileWFC tile in currentNode.possibleTiles)
-                {
-                    if(!possibleSockets.Contains(tile.GetSocket(i)))
-                        possibleSockets.Add(tile.GetSocket(i));
-                }
 
-                neighbourTiles = new(neighbour.possibleTiles);
-                foreach (TileWFC tile in neighbourTiles)
-                {
-                    int neighbourSocket = tile.GetSocket(GetOppositeSide(i));
-                    if(!possibleSockets.Contains(neighbourSocket))
-                        neighbour.possibleTiles.Remove(tile);
-                }
 
-                if(neighbourTiles.Count != neighbour.possibleTiles.Count)
-                {
-                    nodesToCollapse.SortUp(neighbour);
-                    nodesStack.Push(neighbour);
-                }
 
-                possibleSockets.Clear();
-            }
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Node currentNode;
+
+        // while(nodesToCollapse.HeapSize > 0)
+        // {
+        //     currentNode = nodesToCollapse.RemoveFirst();
+
+        //     //set the tile of the current node
+        //     int tileIndex = currentNode.possibleTiles.Count > 1 ? Random.Range(0, currentNode.possibleTiles.Count) : 0;
+        //     currentNode.nodeTile = currentNode.possibleTiles[tileIndex];
+        //     currentNode.ReduceEntropy();
+        //     Instantiate(currentNode.nodeTile.Object, currentNode.nodePos, Quaternion.identity, transform);
+            
+        //     for (int i = 0; i < 4; i++)
+        //     {
+        //         if(TryGetNeighborFromDirection(i, currentNode, out Node neighbour))
+        //             neighbour.collapsedNeighbours++;
+        //     }
+
+        //     if(nodesToCollapse.HeapSize <= 0)
+        //         break;
+
+        //     nodesStack.Push(currentNode);
+            
+        //     //Propagate the collapse to the neighbours
+        //     if(nodesStack.Count > 0)
+        //         PropagateCollapse();
+            
+        // }
     }
 
     int GetOppositeSide(int side) => side + 2 < 4 ? side + 2 : side - 2;
